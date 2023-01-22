9ada0a42ded7073dedebee8f1582d0c29960a513
IyumDeepak <115373833+IyumDeepak@users.noreply.github.com>
2023-01-22 16:28:04 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default{
+    JWT_SECRET: "9fZz3EiXfSt06Q5dQKPOBsCBTEOXsAT7lgW3eqdZf8c=",
+    EMAIL: "warren32@ethereal.email",
+    PASSWORD:"mV2rKNwxkYqkzEZWwj",
+    ATLAS_URI: "mongodb+srv://Deepak:906518@cluster0.c6hkxjh.mongodb.net/?retryWrites=true&w=majority"
+}
